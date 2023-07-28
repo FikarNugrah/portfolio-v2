16c7eb695c8537870475934fb69a3fa798c2f9d7
yafikar nugrah paskah <88127609+FikarNugrah@users.noreply.github.com>
2023-07-28 16:27:12 +0700

    edit: side hero

--- src/components/SIdeHero.jsx
+++ src/components/SIdeHero.jsx
@@ -2,10 +2,10 @@ export default function SideHero() {
   return (
     <>
       <section className="side-hero-section">
-        <div className="top-desc">
+        <div className="top-dscp">
           <h1>Fikarム</h1>
           <h3>
-            Student <span>& Junior Devoloper</span>
+            Student <span> & Junior Devoloper</span>
           </h3>
         </div>
       </section>

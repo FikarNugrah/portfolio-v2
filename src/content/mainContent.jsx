eb7618d878b08415ee9c637212b8e4664bfeeba3
yafikar nugrah paskah <88127609+FikarNugrah@users.noreply.github.com>
2023-08-04 14:01:20 +0700

    re-added: animation

--- src/content/mainContent.jsx
+++ src/content/mainContent.jsx
@@ -1,27 +1,40 @@
 import { AiOutlineHome } from "react-icons/ai";
+import AOS from "aos";
+import "aos/dist/aos.css";
+AOS.init({
+  duration: 1000,
+});
 
 export default function MainContent() {
   return (
     <>
       <div className="main-content" id="home">
-        <p className="introduce">
+        <p className="introduce" data-aos="fade-right" data-aos-duration="1000">
           <AiOutlineHome />
           INTRODUCE
         </p>
-        <p className="intro">
+        <p className="intro" data-aos="fade-right" data-aos-duration="1400">
           Say Hallo! From <span>Fikar</span>, a student and junior developer
           <span>.</span>
         </p>
-        <p className="support-intro">
+        <p
+          className="support-intro"
+          data-aos="fade-right"
+          data-aos-duration="1700"
+        >
           I will develop my skills in the field of web developer, and help
           support me
         </p>
         <div className="status">
-          <div className="started">
+          <div
+            className="started"
+            data-aos="fade-right"
+            data-aos-duration="1400"
+          >
             <h1>4+</h1>
             <p>been studying for 4 months</p>
           </div>
-          <div className="complated">
+          <div className="complated" data-aos="fade-right">
             <h1>30+</h1>
             <p>Has worked on more than 30 projects</p>
           </div>
--- src/content/projectContent.jsx
+++ src/content/projectContent.jsx
@@ -8,37 +8,57 @@ export default function ProjectContent() {
   return (
     <>
       <section className="project-content" id="project">
-        <p className="header-project-content">
+        <p
+          className="header-project-content"
+          data-aos="fade-up"
+          data-aos-duration="1100"
+        >
           <AiOutlineDeploymentUnit />
           PROJECTS
         </p>
-        <p className="top-text-projects">
+        <p
+          className="top-text-projects"
+          data-aos="fade-up"
+          data-aos-duration="1300"
+        >
           my <span>project</span> lately.
         </p>
         <div className="project-areas">
           <div
             className="prjct-area"
-            data-aos="fade-down-right"
-            data-aos-duration="1200"
+            data-aos="fade-up"
+            data-aos-duration="1500"
           >
             <div className="project">
               <img width={300} src={projct1} alt="" />
             </div>
             <p className="time">4 months ago - Random Website</p>
           </div>
-          <div className="prjct-area">
+          <div
+            className="prjct-area"
+            data-aos="fade-up"
+            data-aos-duration="1700"
+          >
             <div className="project">
               <img width={300} src={projct2} alt="" />
             </div>
             <p className="time">3 months ago - Game Store</p>
           </div>
-          <div className="prjct-area">
+          <div
+            className="prjct-area"
+            data-aos="fade-up"
+            data-aos-duration="1900"
+          >
             <div className="project">
               <img width={300} src={projct3} alt="" />
             </div>
             <p className="time">1 months ago - Streaming Movie</p>
           </div>
-          <div className="prjct-area">
+          <div
+            className="prjct-area"
+            data-aos="fade-up"
+            data-aos-duration="2100"
+          >
             <div className="project">
               <img width={300} src={projct4} alt="" />
             </div>
